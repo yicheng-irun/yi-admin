22d2ef08d518b14abf1b0922394f34fc5e891338
yicheng <yicheng-me@qq.com>
2021-12-18 17:33:24 +0800

    feat: jodit编辑器功能完成

--- src/client/pages/model-admin-edit/form-components/string-jodit-editor.vue
+++ src/client/pages/model-admin-edit/form-components/string-jodit-editor.vue
@@ -1,6 +1,5 @@
 <template>
   <div
-
     class="form-component-string-jodit-editor"
   >
     <textarea
@@ -11,96 +10,104 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import { loadStyleFromAssets, loadScriptFromAssets } from '../../../lib/load-file';
+<script setup lang="ts">
+import { computed, onMounted, PropType, ref, watch } from 'vue';
+import { loadStyle, loadScript } from '../../../lib/load-file';
 
-export default defineComponent({
-  model: {
-    prop: 'value',
-    event: 'input',
+const props = defineProps({
+  config: { type: Object as PropType<{
+    placeholder?: string
+    }>, default() {
+    return {
+    };
   },
-  props: {
-    value: {
-      type: String,
-      default: '',
+  },
+  editFormData: {
+    type: Object as PropType<Record<string, unknown>>,
+    default() {
+      return {};
     },
-    config: {
-      type: Object,
-      default() {
-        return {};
+  },
+  name: String,
+  fieldName: String,
+  objectKey: {
+    type: [String, Number] as PropType<string|number>,
+    default: '',
+  },
+});
+
+function handlerInput(value: string) {
+  props.editFormData[props.objectKey] = value;
+}
+
+const textarea = ref<HTMLTextAreaElement>();
+
+const value = computed((): string => {
+  return props.editFormData[props.objectKey] as string || '';
+});
+
+watch(value, (value: string) => {
+  if (joditEditor) {
+    if (joditEditor.value !== value) {
+      joditEditor.value = value;
+    }
+  }
+});
+
+let joditEditor: any = null;
+
+async function initJoditEditor() {
+  if (joditEditor) return;
+  loadStyle('https://cdn.bootcdn.net/ajax/libs/jodit/3.7.2/jodit.min.css');
+  await loadScript('https://cdn.bootcdn.net/ajax/libs/jodit/3.7.2/jodit.min.js');
+
+  // @ts-ignore
+  joditEditor = new window.Jodit(textarea.value, {
+    toolbarButtonSize: 'small',
+    placeholder: props.config.placeholder,
+    allowResizeY: true,
+
+    uploader: {
+      url: 'component-action/',
+      data: {
+        fieldName: props.fieldName,
+        actionName: 'uploader',
+      },
+      getMessage(t: { message: string }) {
+        console.log(t);
+        return t.message;
+      },
+      isSuccess(t: { success: boolean }) {
+        return t.success === true;
       },
     },
-    fieldName: {
-      type: String,
-      default: '',
-    },
-    editFormData: {
-      type: [Object, Array],
-      default() {
-        return {};
-      },
-    },
-  },
-  watch: {
-    value(nv) {
-      if (this.joditEditore && this.joditEditore.value !== nv) {
-        this.joditEditore.value = nv;
-      }
-    },
-  },
-  async mounted() {
-    loadStyleFromAssets('static/jodit/jodit.min.css');
-    await loadScriptFromAssets('static/jodit/jodit.min.js');
-
-    this.joditEditore = new window.Jodit(this.$refs.textarea, {
-      toolbarButtonSize: 'small',
-      placeholder: this.config.placeholder,
-      allowResizeY: true,
-
-      uploader: {
-        url: 'component-action/',
-        data: {
-          fieldName: this.fieldName,
-          actionName: 'uploader',
-        },
-        getMessage(t) {
-          console.log(t);
-          return t.message;
-        },
-        isSuccess(t) {
-          return t.success === true;
-        },
-      },
 
 
-      extraButtons: [
-        // 留着后面慢慢再补充内容
-        // {
-        //    name: '代码语言',
-        //    exec (t, e, o) {
-        //       console.log(t, e, o);
-        //    },
-        //    list: {
-        //       javascript: 'javascript',
-        //       bash: 'bash',
-        //       html: 'html',
-        //       css: 'css',
-        //    },
-        //    tooltip: '代码语言',
-        // },
-      ],
-    });
-    this.joditEditore.events.on('change', () => {
-      this.handleInput(this.joditEditore.value);
-    });
-  },
-  methods: {
-    handleInput(value) {
-      const v = String(value);
-      this.$emit('input', v);
-    },
-  },
+    extraButtons: [
+      // 留着后面慢慢再补充内容
+      // {
+      //    name: '代码语言',
+      //    exec (t, e, o) {
+      //       console.log(t, e, o);
+      //    },
+      //    list: {
+      //       javascript: 'javascript',
+      //       bash: 'bash',
+      //       html: 'html',
+      //       css: 'css',
+      //    },
+      //    tooltip: '代码语言',
+      // },
+    ],
+  });
+
+  joditEditor.events.on('change', () => {
+    handlerInput(joditEditor.value);
+  });
+}
+
+onMounted(() => {
+  initJoditEditor();
 });
 </script>
 
