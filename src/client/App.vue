75ec4ab3f4094cd01f89b48d246c2dfdd1c9e554
ichengzhang <ichengzhang@tencent.com>
2021-12-21 15:01:32 +0800

    chore: 调整naive-ui的primary颜色

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,17 +1,29 @@
 
 <template>
-  <n-message-provider>
-    <message-api ></message-api>
-  </n-message-provider>
-  <n-notification-provider>
-    <notification-api></notification-api>
-  </n-notification-provider>
-  <router-view />
+  <n-config-provider :theme-overrides="themeOverrides">
+    <n-message-provider>
+      <message-api ></message-api>
+    </n-message-provider>
+    <n-notification-provider>
+      <notification-api></notification-api>
+    </n-notification-provider>
+    <router-view />
+  </n-config-provider>
 </template>
 
 <script setup lang="ts">
 import MessageApi from './components/message-api.vue';
 import NotificationApi from './components/notification-api.vue';
+import type { GlobalThemeOverrides } from 'naive-ui';
+
+const themeOverrides: GlobalThemeOverrides = {
+  common: {
+    primaryColor: '#1890ff',
+    primaryColorHover: '#40a9ff',
+    primaryColorPressed: '#096dd9',
+  },
+};
+
 </script>
 
 <style lang="scss">
@@ -20,5 +32,9 @@ import NotificationApi from './components/notification-api.vue';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  >.n-config-provider {
+    height: 100%;
+    width: 100%;
+  }
 }
 </style>
