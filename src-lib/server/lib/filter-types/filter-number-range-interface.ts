8af5aaa5cc396a5885b35860e146fe8292d0e446
ichengzhang <ichengzhang@tencent.com>
2022-01-03 22:12:51 +0800

    修改git ignore

---
+++ src-lib/server/lib/filter-types/filter-number-range-interface.ts
@@ -0,0 +1,5 @@
+import { FilterBaseInterface } from './filter-base-interface';
+
+export interface FilterNumberRangeInterface extends FilterBaseInterface {
+   componentName: 'number-range';
+}
---
+++ src-lib/server/lib/list-types/list-date-time-type.ts
@@ -0,0 +1,9 @@
+import { ListBaseType } from './list-base-type';
+
+
+export class ListDateTimeType extends ListBaseType {
+  /**
+    * 前端的组件名称
+    */
+  public componentName = 'date-time';
+}
